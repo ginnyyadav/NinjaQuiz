d16f29dedcf5af35b450d0aa82937a86892fe909
Ginny Yadav <ginny@tidepool.org>
2020-11-09 18:51:48 -0500

    show score

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const correctAnswers = ['B', 'B', 'B', 'B'];
 const form = document.querySelector('.quiz-form');
+const result = document.querySelector('.result');
 
 form.addEventListener('submit', e => {
     e.preventDefault();
@@ -10,6 +11,10 @@ form.addEventListener('submit', e => {
     userAnswers.forEach((answer, index) => {
         if(answer === correctAnswers[index]){
             score += 25;
+            
         }
     });
-})
+
+    result.querySelector('span').textContent = `${score}%`;
+    result.classList.remove('d-none');
+});
